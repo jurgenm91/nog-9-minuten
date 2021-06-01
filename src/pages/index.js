b65836e512a04ba3b72d884d8b122a25192473ac
jurgenm91 <muls.jurgen@gmail.com>
2021-06-01 22:12:41 +0200

    Spotify link gewijzigd

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ const IndexPage = () => (
      <ul>
      <li className="youtube"> <a href="https://www.youtube.com/channel/UCJpTG0ehUUnPK0z1SAWh-QA"><YouTubeImage /></a></li>
       <li><a href="https://www.youtube.com/channel/UCJpTG0ehUUnPK0z1SAWh-QA"><p>Beluister op YouTube</p></a></li>
-      <li className="spotify"><a href="https://open.spotify.com/album/6EnQdXeP6hkz18nFIuc0nd?si=8HHT6sanTmi3mrUIIS4rbg"><Spotify /></a></li>
+      <li className="spotify"><a href="https://open.spotify.com/show/4I316epb2eDtXsBnuazHl1?si=2oQj3TTaT5W2aWzlKcZLfA"><Spotify /></a></li>
       <li><a href="https://open.spotify.com/album/6EnQdXeP6hkz18nFIuc0nd?si=8HHT6sanTmi3mrUIIS4rbg"><p className="pSpotify">Beluister op Spotify</p></a></li>
       <li className="instagram"><a href="https://www.instagram.com/nog.9.minuten/"><InstagramImage /></a></li>
       <li><a href="https://www.instagram.com/nog.9.minuten/"><p className="pInstagram">Volg ons op Instagram</p></a></li>
